082ff4c41ed957856b00ecf7229b6dbaebce438c
chari <chari@ASUSmishiro.elecom>
2025-04-04 18:16:48 +0900

    ホテルル８－２まで

---
+++ moattravel/src/main/java/com/example/moattravel/repository/HouseRepository.java
@@ -0,0 +1,8 @@
+package com.example.moattravel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.moattravel.entity.House;
+
+public interface HouseRepository extends JpaRepository<House, Integer> {
+}
