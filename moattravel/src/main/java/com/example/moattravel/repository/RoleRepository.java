cbc06d8cd4f6efab51fdc33a83b3d746b00cc34e
chari <chari@ASUSmishiro.elecom>
2025-04-12 00:07:07 +0900

    23章まで

--- moattravel/src/main/java/com/example/moattravel/repository/RoleRepository.java
+++ moattravel/src/main/java/com/example/moattravel/repository/RoleRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.moattravel.entity.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-	//public Page<Role> findByNameLike(String keyword, Pageable pageale);
+	public Role findByName(String name);
 }
