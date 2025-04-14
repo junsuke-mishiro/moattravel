0eb56da483547e6eba9b0c7bcd51b8a32a7da74b
chari <chari@ASUSmishiro.elecom>
2025-04-14 19:15:12 +0900

    26章まで

--- moattravel/src/main/java/com/example/moattravel/service/UserService.java
+++ moattravel/src/main/java/com/example/moattravel/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.moattravel.entity.Role;
 import com.example.moattravel.entity.User;
 import com.example.moattravel.form.SignupForm;
+import com.example.moattravel.form.UserEditForm;
 import com.example.moattravel.repository.RoleRepository;
 import com.example.moattravel.repository.UserRepository;
 
@@ -35,9 +36,23 @@ public class UserService {
 		user.setEmail(signupForm.getEmail());
 		user.setPassword(passwordEncoder.encode(signupForm.getPassword()));
 		user.setRole(role);
-		user.setEnabled(true);
+		user.setEnabled(false);
 
 		return userRepository.save(user);
+	}
+	
+	@Transactional
+	public void update(UserEditForm userEditForm) {
+		User user = userRepository.getReferenceById(userEditForm.getId());
+
+		user.setName(userEditForm.getName());
+		user.setFurigana(userEditForm.getFurigana());
+		user.setPostalCode(userEditForm.getPostalCode());
+		user.setAddress(userEditForm.getAddress());
+		user.setPhoneNumber(userEditForm.getPhoneNumber());
+		user.setEmail(userEditForm.getEmail());
+
+		userRepository.save(user);
 	}
 
 	//メールアドレスが登録済みかどうかをチェックする
@@ -49,5 +64,18 @@ public class UserService {
 	//パスワードとパスワード（確認用）の入力値が一致するかどうかをチェックする
 	public boolean isSamePassword(String password, String passwordConfirmation) {
 		return password.equals(passwordConfirmation);
+	}
+
+	//ユーザーを有効にする
+	@Transactional
+	public void enableUser(User user) {
+		user.setEnabled(true);
+		userRepository.save(user);
+	}
+	
+	//メールアドレスが変更されたかどうかをチェックする
+	public boolean isEmailChanged(UserEditForm userEditForm) {
+		User currentUser = userRepository.getReferenceById(userEditForm.getId());
+		return !userEditForm.getEmail().equals(currentUser.getEmail());
 	}
 }
